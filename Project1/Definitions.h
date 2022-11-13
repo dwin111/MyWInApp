f47853ea2504a75971245dfee062b3c01089a1cc
Dmytro <47865952+dwin111@users.noreply.github.com>
2022-11-14 00:25:37 +0200

    Update Definitions.h

--- Project1/Definitions.h
+++ Project1/Definitions.h
@@ -4,15 +4,15 @@
 
 #define BINDING_STYLE		WS_VISIBLE | WS_CHILD  
 
-#define ON_MENU_CLICK_1	    1
-#define ON_MENU_CLICK_2	    2
-#define ON_MENU_CLICK_3	    3
+#define ON_MENU_CLICK_1	      1
+#define ON_MENU_CLICK_2	      2
+#define ON_MENU_CLICK_3	      3
 
-#define ON_BUTTON_CLICK_1	4
-#define ON_BUTTON_CLICK_2	5
-#define ON_BUTTON_CLICK_3	6
+#define ON_BUTTON_CLICK_1	    4
+#define ON_BUTTON_CLICK_2	    5
+#define ON_BUTTON_CLICK_3	    6
 
-#define ON_CONNECTED        7
+#define ON_CONNECTED         7
 
 #define TEXT_BUFFER_SIZE    256
 
