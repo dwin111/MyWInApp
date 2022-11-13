debe08b00b80b7915e89781170d2ffba6f561523
Dmytro <47865952+dwin111@users.noreply.github.com>
2022-11-14 01:04:40 +0200

    Update Definitions.h

--- Project1/Definitions.h
+++ Project1/Definitions.h
@@ -4,18 +4,20 @@
 
 #define BINDING_STYLE		WS_VISIBLE | WS_CHILD  
 
-#define ON_MENU_CLICK_1	      1
-#define ON_MENU_CLICK_2	      2
-#define ON_MENU_CLICK_3	      3
+#define ON_MENU_CLICK_1	    1
+#define ON_MENU_CLICK_2	    2
+#define ON_MENU_CLICK_3	    3
 
-#define ON_BUTTON_CLICK_1	    4
-#define ON_BUTTON_CLICK_2	    5
-#define ON_BUTTON_CLICK_3	    6
+#define ON_BUTTON_CLICK_1	  4
+#define ON_BUTTON_CLICK_2	  5
+#define ON_BUTTON_CLICK_3  	6
 
-#define ON_CONNECTED         7
+#define ON_CONNECTED        7
+#define ON_SERIAL_REFRESH   8
 
 #define TEXT_BUFFER_SIZE    256
-
+#define COM_SELECT_INDEX    120
+#define COM_PORT_AMOINT     50
 
 
 char Buffer[TEXT_BUFFER_SIZE];
@@ -24,6 +26,9 @@ HWND Lable1;
 HWND Lable2;
 HWND TextBox1;
 HWND TextBox2;
+
+HMENU ComPortSubMenu;
+HMENU ComPortListMenu;
 
 
 char fileName[260];
