747b13ee60fc5705fa9b79abb795eba29a5551a2
Dmytro <47865952+dwin111@users.noreply.github.com>
2022-11-15 01:07:52 +0200

    Update Definitions.h

--- Project1/Definitions.h
+++ Project1/Definitions.h
@@ -8,12 +8,19 @@
 #define ON_MENU_CLICK_2	    2
 #define ON_MENU_CLICK_3	    3
 
-#define ON_BUTTON_CLICK_1	  4
-#define ON_BUTTON_CLICK_2	  5
-#define ON_BUTTON_CLICK_3   6
+#define ON_BUTTON_CLICK_1	4
+#define ON_BUTTON_CLICK_2	5
+#define ON_BUTTON_CLICK_3	6
 
 #define ON_CONNECTED        7
 #define ON_SERIAL_REFRESH   8
+
+#define ON_BUTTON_CLICK_4   9
+
+
+#define INDEX_COLOR_R		310
+#define INDEX_COLOR_G		311
+#define INDEX_COLOR_B		312
 
 #define TEXT_BUFFER_SIZE    256
 #define COM_SELECT_INDEX    120
@@ -26,18 +33,27 @@ HWND Lable1;
 HWND Lable2;
 HWND TextBox1;
 HWND TextBox2;
+HWND TextBox3;
+HWND TextBox4;
 
 HMENU ComPortSubMenu;
 HMENU ComPortListMenu;
 
+PAINTSTRUCT ps;
+RECT windowRectangle;
+HBRUSH brushRectangle;
 
 char fileName[260];
 OPENFILENAMEA ofn;
 
-bool isConnected = false;
+volatile bool isConnected = false;
+volatile bool isThreading = true;
 int selectedPort = 3;
 int targetBaudRate = 9600;
+
 HANDLE connectedPort;
+
+HANDLE readThread;
 
 LRESULT CALLBACK MainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
 WNDCLASS NewWindowsClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
