5aa81221685dc46b6aeb64342e1dd0f427e140a6
Dmytro <47865952+dwin111@users.noreply.github.com>
2022-11-15 01:07:18 +0200

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -15,31 +15,37 @@ Font Name: Soft
 
 */
 
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR argd, int ncmdshow)
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst,
+	LPSTR argd, int ncmdshow)
 {
-	WNDCLASS MainClass = NewWindowsClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst, LoadIcon(NULL, IDI_APPLICATION),
-		L"MainWinClass", MainProcedure);
+	WNDCLASS MainClass = NewWindowsClass(
+		(HBRUSH)COLOR_WINDOW, 
+		LoadCursor(NULL, IDC_ARROW),
+		hInst,
+		LoadIcon(NULL, IDI_APPLICATION),
+		L"MainWinClass", 
+		MainProcedure);
 
 	if (!RegisterClassW(&MainClass))
 	{
 		return -1;
 	}
+    MSG MainMessage = { 0 };
 
-	MSG MainMessage = { 0 };
-
-	CreateWindow(L"MainWinClass", L"First Window", (WS_OVERLAPPEDWINDOW | WS_VISIBLE), 100, 100, 500, 250, NULL, NULL, NULL, NULL);
+	CreateWindow(L"MainWinClass", L"First Window", (WS_OVERLAPPEDWINDOW | WS_VISIBLE), 100, 100,
+		500, 250, NULL, NULL, NULL, NULL);
 
 	while (GetMessage(&MainMessage, NULL, NULL, NULL))
 	{
 		TranslateMessage(&MainMessage);
 		DispatchMessage(&MainMessage);
-
 	}
+	TerminateThread(readThread, 0);
 	return 0;
 }
 
-WNDCLASS NewWindowsClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure)
-{
+WNDCLASS NewWindowsClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst,
+	HICON Icon, LPCWSTR Name, WNDPROC Procedure){
 	WNDCLASS NWC = { 0 };
 
 	NWC.hIcon = Icon;
@@ -52,17 +58,23 @@ WNDCLASS NewWindowsClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON 
 	return NWC;
 }
 
-LRESULT CALLBACK MainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
-{
-	if (isConnected) {
-		SerialRead();
-	}
-	switch (msg){
+void ExitSoftware(void){
+	isConnected = false;
+	isThreading = false;
+	CloseHandle(connectedPort);
+	CloseHandle(readThread);
+	ExitThread(0);
+	PostQuitMessage(0);
+}
+
+LRESULT CALLBACK MainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp){
+	switch (msg) {
 	case WM_CREATE:
 		MainWindAddMenus(hWnd);
 		MainWindAddWidgets(hWnd);
 		SetOpenFileParams(hWnd);
 		SerialUpdate();
+		readThread = CreateThread(NULL, 0, SrialRead, NULL, 0, NULL);
 		break;
 	case WM_COMMAND:
 		if ((wp >= COM_SELECT_INDEX) && (wp < COM_SELECT_INDEX + COM_PORT_AMOINT)) {
@@ -70,30 +82,28 @@ LRESULT CALLBACK MainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 			SetWindowStatus("PORT" + std::to_string(selectedPort));
 			SerialUpdate();
 		}
-
 		switch (wp)
 		{
 		case ON_MENU_CLICK_1:
-			MessageBoxA(hWnd, "THIS IS File", "THIS IS File", MB_OK);
+		    MessageBoxA(hWnd, "THIS IS File", "THIS IS File", MB_OK);
 			break;
 		case ON_MENU_CLICK_2:
-			MessageBoxA(hWnd, "THIS IS Window", "THIS IS Window", MB_OK);
+		    MessageBoxA(hWnd, "THIS IS Window", "THIS IS Window", MB_OK);
 			break;
 		case ON_MENU_CLICK_3:
-			MessageBoxA(hWnd, "THIS IS View", "THIS IS View", MB_OK);
+		    MessageBoxA(hWnd, "THIS IS View", "THIS IS View", MB_OK);
 			break;
 		case ON_BUTTON_CLICK_1:
-			SetWindowTextA(TextBox1, "");
-			SetWindowTextA(TextBox2, "");
+		    SetWindowTextA(TextBox1, "");
 			break;
 		case ON_BUTTON_CLICK_2:
 			if (GetOpenFileNameA(&ofn)) {
-				LoadData(fileName, TextBox2);
-			}		
+				LoadData(fileName, TextBox1);
+			}
 			break;
 		case ON_BUTTON_CLICK_3:
 			if (GetSaveFileNameA(&ofn)) {
-				SaveData(fileName, TextBox2);
+				SaveData(fileName, TextBox1);
 			}
 			break;
 		case ON_CONNECTED:
@@ -102,11 +112,26 @@ LRESULT CALLBACK MainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 		case ON_SERIAL_REFRESH:
 			SerialUpdate();
 			break;
+		case ON_BUTTON_CLICK_4:
+			brushRectangle = CreateSolidBrush(
+				RGB(GetDlgItemInt(hWnd, INDEX_COLOR_R, (BOOL*)false, (BOOL)false),
+					GetDlgItemInt(hWnd, INDEX_COLOR_G, (BOOL*)false, (BOOL)false),
+					GetDlgItemInt(hWnd, INDEX_COLOR_B, (BOOL*)false, (BOOL)false)
+				));
+			RedrawWindow(hWnd, NULL, NULL, RDW_UPDATENOW | RDW_INVALIDATE);
+			break;
 		}
 
 		break;
+	case WM_PAINT:
+		BeginPaint(hWnd, &ps);
+
+		FillRect(ps.hdc, &windowRectangle, brushRectangle);
+
+		EndPaint(hWnd, &ps);
+		break;
 	case WM_DESTROY:
-		PostQuitMessage(0);
+		ExitSoftware();
 		break;
 	default:
 		return DefWindowProc(hWnd, msg, wp, lp);
@@ -115,14 +140,12 @@ LRESULT CALLBACK MainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 	}
 }
 
-void MainWindAddMenus(HWND hWnd)
-{
+void MainWindAddMenus(HWND hWnd){
 	HMENU RootMenu = CreateMenu(); //Созданеи TollBar
 	HMENU SubMenu = CreateMenu(); //Созданеи TollBar
 
 	ComPortSubMenu = CreateMenu();
 	ComPortListMenu = CreateMenu();
-	
 
 	AppendMenu(SubMenu, MF_STRING, ON_BUTTON_CLICK_3, L"Save");
 	AppendMenu(SubMenu, MF_STRING, ON_BUTTON_CLICK_2, L"Load");
@@ -139,25 +162,29 @@ void MainWindAddMenus(HWND hWnd)
 	SetMenu(hWnd, RootMenu);
 }
 
-void MainWindAddWidgets(HWND hWnd)
-{
-	Lable1 = CreateWindowA("static", "Hellow", BINDING_STYLE | ES_CENTER, 5, 5, 490, 20, hWnd, NULL, NULL, NULL); // LABLE
+void MainWindAddWidgets(HWND hWnd){
+	windowRectangle = { 5 + 470, 40, 5, 40 + 120 };
 
-	Lable2 = CreateWindowA("static", "Hellow", BINDING_STYLE | ES_CENTER, 5, 150, 490, 20, hWnd, NULL, NULL, NULL); // LABLE
+	Lable1 = CreateWindowA("static", "Hellow", BINDING_STYLE | ES_CENTER, 5, 5, 150, 30, hWnd, NULL, NULL, NULL); // LABLE	
 
-	TextBox1 = CreateWindowA("edit", "Hellow", BINDING_STYLE | ES_MULTILINE | WS_VSCROLL, 5, 50, 490, 100, hWnd, NULL, NULL, NULL); // TEXTBOX
+	TextBox1 = CreateWindowA("edit", "", BINDING_STYLE | ES_MULTILINE | WS_VSCROLL, 5, 30, 490, 90, hWnd, NULL, NULL, NULL); // TEXTBOX
 
-	TextBox2 = CreateWindowA("edit", "Hellow", BINDING_STYLE | ES_MULTILINE | WS_VSCROLL, 5, 200, 490, 100, hWnd, (HMENU)200, NULL, NULL); // TEXTBOX
+	TextBox2 = CreateWindowA("edit", "", BINDING_STYLE | ES_MULTILINE | WS_VSCROLL, 170, 130, 100, 30, hWnd, (HMENU)INDEX_COLOR_R, NULL, NULL); // TEXTBOX
 
-	CreateWindowA("button", "Hellow", BINDING_STYLE | ES_CENTER, 5, 20, 490, 30, hWnd, (HMENU)ON_BUTTON_CLICK_1, NULL, NULL); // BUTTON
+	TextBox3 = CreateWindowA("edit", "", BINDING_STYLE | ES_MULTILINE | WS_VSCROLL, 170 + 110, 130, 100, 30, hWnd, (HMENU)INDEX_COLOR_G, NULL, NULL); // TEXTBOX
 
-	CreateWindowA("button", "Read", BINDING_STYLE | ES_CENTER, 500, 200, 50, 30, hWnd, (HMENU)ON_BUTTON_CLICK_2, NULL, NULL); // BUTTON
+	TextBox4 = CreateWindowA("edit", "", BINDING_STYLE | ES_MULTILINE | WS_VSCROLL, 170 + 215, 130, 100, 30, hWnd, (HMENU)INDEX_COLOR_B, NULL, NULL); // TEXTBOX
 
-	CreateWindowA("button", "Save", BINDING_STYLE | ES_CENTER, 1000, 200, 50, 30, hWnd, (HMENU)ON_BUTTON_CLICK_3, NULL, NULL); // BUTTON
+	CreateWindowA("button", "Read", BINDING_STYLE | ES_CENTER, 5, 130, 50, 30, hWnd, (HMENU)ON_BUTTON_CLICK_2, NULL, NULL); // BUTTON
+
+	CreateWindowA("button", "Save", BINDING_STYLE | ES_CENTER, 60, 130, 50, 30, hWnd, (HMENU)ON_BUTTON_CLICK_3, NULL, NULL); // BUTTON
+
+	CreateWindowA("button", "Clear", BINDING_STYLE | ES_CENTER, 115, 130, 50, 30, hWnd, (HMENU)ON_BUTTON_CLICK_1, NULL, NULL); // BUTTON
+
+	CreateWindowA("button", "Color", BINDING_STYLE | ES_CENTER, 430, 0, 50, 30, hWnd, (HMENU)ON_BUTTON_CLICK_4, NULL, NULL); // BUTTON
 }
 
-void SaveData(LPCSTR path, HWND TextBox)
-{
+void SaveData(LPCSTR path, HWND TextBox){
 	HANDLE FileToSave = CreateFileA(
 		path,
 		GENERIC_WRITE,
@@ -181,8 +208,7 @@ void SaveData(LPCSTR path, HWND TextBox)
 	delete[] data;
 }
 
-void LoadData(LPCSTR path, HWND TextBox)
-{
+void LoadData(LPCSTR path, HWND TextBox){
 	HANDLE FileToLoad = CreateFileA(
 		path,
 		GENERIC_READ,
@@ -199,8 +225,7 @@ void LoadData(LPCSTR path, HWND TextBox)
 
 }
 
-void SetOpenFileParams(HWND hWnd)
-{
+void SetOpenFileParams(HWND hWnd){
 	ZeroMemory(&ofn, sizeof(ofn));
 
 	ofn.lStructSize = sizeof(ofn);
@@ -214,7 +239,7 @@ void SetOpenFileParams(HWND hWnd)
 	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
 }
 
-void SetWindowStatus(std::string status)
-{
+void SetWindowStatus(std::string status){
+
 	SetWindowTextA(Lable1, status.c_str());
 }
